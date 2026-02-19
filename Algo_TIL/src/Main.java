5c8ede9d5f7ea4d5378a4c42827cb1d4e0d95b1a
okccc5 <okccc5@naver.com>
2026-02-19 13:09:51 +0900

    update

--- Algo_TIL/src/Main.java
+++ Algo_TIL/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  * 4353 문자열 제곱
@@ -11,47 +12,41 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		// 특정 문자열이 주어졌을 때
-		// 그 문자열이 어떤 문자열 a를 몇 제곱한지 구하여라 + 최대값으로 구하셈
-		//
-		
-
 		while (true) {
 			String str = br.readLine();
 			if (str.equals(".")) {
-				System.out.println(sb.toString().trim());
 				break;
 			}
-
 			sb.append(findPi(str)).append("\n");
-
 		}
-
+		System.out.println(sb.toString().trim());
 	}
 
-	private static Integer findPi(String str) {
+	// 실패 탐지
+	private static int findPi(String str) {
 		int idx = 0;
 		int len = str.length();
 		int[] pi = new int[len];
-		int max = 1;
 
 		for (int i = 1; i < len; i++) {
-			while (idx > 1 && str.charAt(i) != str.charAt(idx)) {
+			while (idx > 0 && str.charAt(i) != str.charAt(idx)) {
 				idx = pi[idx - 1];
 			}
 
 			if (str.charAt(i) == str.charAt(idx)) {
 				pi[i] = ++idx;
 			}
-
 		}
 
-		for (int i = 0; i < len; i++) {
-			if (max < pi[i]) {
-				max = pi[i];
-			}
+		int lastPi = pi[len - 1]; // 가장 긴 접두사=접미사 길이
+		int unitLen = len - lastPi; // 반복 마디의 최소 길이 후보
+
+		// 나머지가 0이라면 제곱의 가능성을 가진 애들
+		if (len % unitLen == 0) {
+			return len / unitLen;
+		} else {
+			return 1;
 		}
 
-		return max;
 	}
 }
